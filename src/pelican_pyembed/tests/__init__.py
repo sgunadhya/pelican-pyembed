4de241a2dc93068d8e390f2d0a37dbb4e93ad512
Sushant Srivastava <sushant.srivastav@gmail.com>
2014-12-20 14:47:02 +0530

    Added template project

---
+++ src/pelican_pyembed/tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+pelican-pyembed: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2014, Sushant Srivastava
+Licensed under MIT
+'''
---
+++ src/pelican_pyembed/tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+pelican-pyembed: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2014, Sushant Srivastava
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
